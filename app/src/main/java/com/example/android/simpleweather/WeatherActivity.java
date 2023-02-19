486edde86bd666d6410e7b14ddb6617952e6b0b9
Aaron Pradhan <12a.pradhan@gmail.com>
2023-02-18 23:34:28 -0500

    Clean up weather activity

--- app/src/main/java/com/example/android/simpleweather/WeatherActivity.java
+++ app/src/main/java/com/example/android/simpleweather/WeatherActivity.java
@@ -16,97 +16,55 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class WeatherActivity extends AppCompatActivity implements View.OnClickListener {
+public class WeatherActivity extends AppCompatActivity {
 
     public RecyclerView weatherRecyclerView;
     public List<WeatherModel> weatherList = new ArrayList<>();
-    RecyclerView.Adapter adapter;
-
-    private WeatherModel[] testArray;
-
-    public void setLoading(Boolean loading) {
-        this.loading = loading;
-    }
-
+    RecyclerView.Adapter<WeatherViewHolder> adapter;
     public Boolean loading;
-
     public Handler textHandler;
-
     private Button search;
-
     private RunnableThread run;
-
     public EditText editTextTextPersonName;
-
     public TextView zip;
-
     public TextView loadingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new
-                    StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
-
+        StrictMode.ThreadPolicy policy = new
+                StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         loading = false;
-        weatherList.add(new WeatherModel("12/10/2024", "Winter", "F", 50, 70));
+
         setContentView(R.layout.activity_weather);
         weatherRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         weatherRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         adapter = new WeatherAdapter(weatherList, this);
-        //Associates the adapter with the RecyclerView
+        // Associates the adapter with the RecyclerView
         weatherRecyclerView.setAdapter(adapter);
 
         search = (Button) findViewById(R.id.search);
-        search.setOnClickListener(this);
+        search.setOnClickListener(v -> {
+            if (v.getId() == R.id.search) {
+                try {
+                    getWeather();
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
         editTextTextPersonName = (EditText) findViewById(R.id.zip_code_edit_text);
         textHandler = new Handler();
         zip = (TextView) findViewById(R.id.zip_code_tv);
         loadingText = (TextView) findViewById(R.id.loadingText);
     }
 
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.search) {
-            try {
-                getWeather();
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
     private void getWeather() throws FileNotFoundException {
-
-
-
-        /*String query = editTextTextPersonName.getText().toString();
-
-        WeatherModel[]  testArray = {new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)
-                , new WeatherModel("12/10/2023", "Sunny", "F", 20, 40)};
-
-        weatherList.addAll(Arrays.asList(testArray));*/
-       run = new RunnableThread(this);
+        run = new RunnableThread(this);
         new Thread(run).start();
-
     }
-
-
 }
-

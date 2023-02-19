9a04827cf37df6ba75c252b469db4c33cbf68f49
amarpriyesh <63479437+amarpriyesh@users.noreply.github.com>
2023-02-19 14:20:52 -0500

    Hourly api changes

--- app/src/main/java/com/example/android/simpleweather/WeatherActivity.java
+++ app/src/main/java/com/example/android/simpleweather/WeatherActivity.java
@@ -1,63 +1,18 @@
 package com.example.android.simpleweather;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import com.example.android.simpleweather.databinding.ActivityWeatherBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class WeatherActivity extends AppCompatActivity {
 
-    ActivityWeatherBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityWeatherBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-
-        binding.bottomNav.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.daily:
-                    replaceFragment(new WeatherDailyFragment());
-                    return true;
-                case R.id.current:
-                    replaceFragment(new WeatherCurrentFragment());
-                    return true;
-            }
-            return false;
-        });
+        setContentView(R.layout.activity_weather);
     }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container_view_weather, fragment);
-        fragmentTransaction.commit();
-    }
-
-//    @Override
-//    public void onSaveInstanceState(Bundle savedInstanceState) {
-//        super.onSaveInstanceState(savedInstanceState);
-//        savedInstanceState.putInt("SelectedItemId", binding.bottomNav.getSelectedItemId());
-//    }
-//
-//    @Override
-//    public void onRestoreInstanceState(Bundle savedInstanceState) {
-//        super.onRestoreInstanceState(savedInstanceState);
-//        int selectedItemId = savedInstanceState.getInt("SelectedItemId");
-//        binding.bottomNav.setSelectedItemId(selectedItemId);
-//    }
-
 }
